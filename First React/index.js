1560c4c8a4a1aeb7d2e8643f89f4456fce3fce01
ShinYoung-Kim <lukey0515@naver.com>
2023-04-22 12:07:03 +0900

    Feat: custom component

--- First React/index.js
+++ First React/index.js
@@ -1,3 +1,23 @@
-ReactDOM.render(<h1>Hello, everyone!</h1>, document.getElementById("root"));
-//ReactDOM.render : react로 render하게 해주는 거
-//argument로 what과 where을 넣어줌
+// ReactDOM.render(<h1>Hello, everyone!</h1>, document.getElementById("root"));
+// //ReactDOM.render : react로 render하게 해주는 거
+// //argument로 what과 where을 넣어줌
+// //append or append child와 유사한 개념
+
+// ReactDOM.render(
+//     <ul><li>Thing 1</li><li>Thing 2</li></ul>,
+//     document.getElementById("root")
+// );
+
+//custom component
+function MainContent() {
+    return (
+        <h1>I'm learning React!</h1>
+    )
+};
+
+ReactDOM.render(
+    <div>
+        <MainContent />
+    </div>,
+    document.getElementById("root")
+);
