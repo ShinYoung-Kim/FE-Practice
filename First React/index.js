31b28d1946dda04c8c35d4ef48abcee10e1912db
ShinYoung-Kim <lukey0515@naver.com>
2023-04-22 20:54:23 +0900

    Feat: nav items 추가

--- First React/index.js
+++ First React/index.js
@@ -180,8 +180,13 @@ _source : null
 function Header() { //children component
     return (
         <header>
-            <nav>
-                <img src="./react-logo.png" width="40px" />
+            <nav className="nav">
+                <img src="./react-logo.png" className="nav-logo" />
+                <ul className="nav-items">
+                    <li>Pricing</li>
+                    <li>About</li>
+                    <li>Contact</li>
+                </ul>
             </nav>
         </header>
     )
