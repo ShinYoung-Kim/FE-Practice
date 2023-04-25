b91c7ddf0e518f8e489fa44720a4021430cf9c88
ShinYoung-Kim <lukey0515@naver.com>
2023-04-25 20:58:48 +0900

    Feat: render app component

--- practice1/my-app/src/App.js
+++ practice1/my-app/src/App.js
@@ -2,4 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Navbar from './components/Navbar';
 import Main from './components/Main';
+import "./index.css";
 
+function App() {
+    return (
+        <div>
+            <Navbar/>
+            <Main/>
+        </div>
+    )
+}
+
+ReactDOM.render(<App/>, document.getElementById("root"));
