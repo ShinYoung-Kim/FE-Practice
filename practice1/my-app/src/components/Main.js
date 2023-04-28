a9ef3cbdc9768f794f1e5c0f0127fc4585da0b59
ShinYoung-Kim <lukey0515@naver.com>
2023-04-28 14:09:30 +0900

    Feat: main component 스타일 지정

--- practice1/my-app/src/components/Main.js
+++ practice1/my-app/src/components/Main.js
@@ -2,6 +2,15 @@ import react from 'react';
 
 export default function Main() {
     return (
-        <h1>This is Main part!</h1>
+       <main>
+        <h1 className='main--title'>Fun facts about React</h1>
+        <ul>
+            <li>Was first released in 2013</li>
+            <li>Was originaly created by Jordan Walke</li>
+            <li>Has well over 100K stars on Github</li>
+            <li>Is maintained by Facebook</li>
+            <li>Powers thousands of enterprise</li>
+        </ul>
+       </main> 
     );
 }
