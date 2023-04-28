7a7fbea209072f6f5ba0595eb0be71f3bcd0aec2
ShinYoung-Kim <lukey0515@naver.com>
2023-04-28 17:49:16 +0900

    Feat: 리액트에서 state 변경

--- CodingApple/my-app/src/App.js
+++ CodingApple/my-app/src/App.js
@@ -4,13 +4,15 @@ import {useState} from 'react';
 
 function App() {
   let [first, firstFunction] = useState(['남자 코트 추천', '남자 코트 추천1', '남자 코트 추천2']);
+  let [count, countFunction] = useState(0);
   return (
     <div className="App">
       <div className="black-nav">
         <div>개발 Blog</div>
       </div>
+      <button onClick={() => {firstFunction(['여자 코트 추천', ...first.splice(1)])}}></button>
       <div className="list">
-        <h3> {first[0]}</h3>
+        <h3> {first[0]} <span onClick={() => { countFunction(count + 1) }}>😊</span> {count} </h3>
         <p>2월 17일 발행</p>
         <hr/>
       </div>
