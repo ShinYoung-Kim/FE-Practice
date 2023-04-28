59110833f9adc748fca8c13136d3ec1d516eca8c
ShinYoung-Kim <lukey0515@naver.com>
2023-04-28 16:28:00 +0900

    !HOTFIX: Compiled with problems:
    react__WEBPACK_IMPORTED_MODULE_2___default is not a function or its return value is not iterable
    TypeError: react__WEBPACK_IMPORTED_MODULE_2___default is not a function or its return value is not iterable

--- CodingApple/my-app/src/App.js
+++ CodingApple/my-app/src/App.js
@@ -1,15 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
+import useState from 'react';
 
 function App() {
-  let posts = "강남 고기 맛집";
+  let [글제목, 글제목변경] = useState('남자 코트 추천');
   return (
     <div className="App">
       <div className="black-nav">
         <div>개발 Blog</div>
       </div>
       <div className="list">
-        <h3> { posts }</h3>
+        <h3> { 글제목 }</h3>
         <p>2월 17일 발행</p>
         <hr/>
       </div>
