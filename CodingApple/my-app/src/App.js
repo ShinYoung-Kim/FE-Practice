8ae6bc4d2bf9d15501e05b45535b2caa9fc62aa1
ShinYoung-Kim <lukey0515@naver.com>
2023-04-28 17:06:48 +0900

    Feat: state 실습 적용

--- CodingApple/my-app/src/App.js
+++ CodingApple/my-app/src/App.js
@@ -3,14 +3,24 @@ import './App.css';
 import {useState} from 'react';
 
 function App() {
-  let [글제목, 글제목변경] = useState('남자 코트 추천');
+  let [first, firstFunction] = useState(['남자 코트 추천', '남자 코트 추천1', '남자 코트 추천2']);
   return (
     <div className="App">
       <div className="black-nav">
         <div>개발 Blog</div>
       </div>
       <div className="list">
-        <h3> { 글제목 }</h3>
+        <h3> {first[0]}</h3>
+        <p>2월 17일 발행</p>
+        <hr/>
+      </div>
+      <div className="list">
+        <h3> {first[1]}</h3>
+        <p>2월 17일 발행</p>
+        <hr/>
+      </div>
+      <div className="list">
+        <h3> {first[2]}</h3>
         <p>2월 17일 발행</p>
         <hr/>
       </div>
