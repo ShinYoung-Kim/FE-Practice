a76c30183a40ab9849b18966506d2556c8dc3202
ShinYoung-Kim <lukey0515@naver.com>
2023-06-16 19:31:13 +0900

    Feat: hover 이벤트 추가

--- bookmark/src/components/Template1List.js
+++ bookmark/src/components/Template1List.js
@@ -1,11 +1,11 @@
-import Template1 from "./Template1";
+import ArticleTemplate from './ArticleTemplate';
 import './Template1List.scss';
 
 const Template1List = ({ templates }) => {
     return (
         <div className="Template1List">
             {templates.map(template => (
-                <Template1 props={ template } key={ template.id }/>
+                <ArticleTemplate props={ template } key={ template.id }/>
             ))}
         </div>
     );
